64026f54df9747418f6bc96c9137a49a70d9c2b3
Jens Heijdenberg <gusheijje@student.gu.se>
2023-05-18 14:58:29 +0200

    ordna multicore, fixa åäö m.m.

--- prototyp/topic/topic.py
+++ prototyp/topic/topic.py
@@ -1,50 +1,104 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
+import time
 import gensim
 from gensim.utils import simple_preprocess
 from gensim.corpora import Dictionary
-from gensim.models import LdaModel
+from gensim.models import LdaMulticore
 from nltk.corpus import stopwords
-from gensim.models.phrases import Phrases
+from gensim.models.phrases import Phrases, Phraser
+import multiprocessing
 
-# Read CSV file
-data = pd.read_csv("job_listings.csv", on_bad_lines='skip')
+def main():
+    # Specify the maximum number of rows to load
+    max_rows = None
+    # Define the filtering value
+    jobb_filter = "Systemutvecklare/Programmerare" # jobb_filter ="" kommer att inklusera alla värden
 
-# Extract the "description.text" column
-documents = data["description.text"].tolist()
 
-# Add custom stop words
-swedish_stopwords = set(stopwords.words("swedish"))
-swedish_stopwords.update(["du", "är", "med", "att", "och", "som", "en", "vi", "pa", "har", "inom", "dig", "av", 'kommer'])  # Add additional stop words
+    # Read CSV file
+    print("Laddar data")
+    data = pd.read_csv("job_listings.csv", on_bad_lines='skip', nrows=max_rows)
 
-# Preprocess the text data
-def preprocess(text):
-    # Tokenize the text into individual words
-    tokens = simple_preprocess(text, deacc=True)
+    filtered_data = data[data['occupation.label'].str.contains(jobb_filter, case=False, na=False)]
 
-    # Remove stopwords
-    tokens = [token for token in tokens if token not in swedish_stopwords]
+    # Extract the "description.text" column
+    documents = filtered_data["description.text"].tolist()
+    
+    # Add custom stop words
+    print("Lägger till stopord")
+    swedish_stopwords = set(stopwords.words("swedish"))
 
-    return tokens
 
-# Apply preprocessing to each document
-preprocessed_documents = [preprocess(doc) for doc in documents]
+    # Load the stop word list from file
+    stopwords_file = "stopwords.txt"
+    with open(stopwords_file, "r", encoding="utf-8") as file:
+        stop_words = [word.strip() for word in file.readlines()]
 
-# Create bigram and trigram models
-bigram = Phrases(preprocessed_documents, min_count=5, threshold=100)
-trigram = Phrases(bigram[preprocessed_documents], min_count=5, threshold=100)
+    swedish_stopwords.update(stop_words)
 
-# Apply bigram and trigram models to documents
-preprocessed_documents = [trigram[bigram[doc]] for doc in preprocessed_documents]
+    # Preprocess the text data
+    print("Prossesar text")
+    def preprocess(text):
+        # Tokenize the text into individual words
+        tokens = simple_preprocess(text, deacc=False)
 
-# Create a dictionary and corpus for the LDA model
-dictionary = Dictionary(preprocessed_documents)
-corpus = [dictionary.doc2bow(doc) for doc in preprocessed_documents]
+        # Remove stopwords
+        tokens = [token for token in tokens if token not in swedish_stopwords]
+        return tokens
 
-# Train the LDA model
-num_topics = 5
-lda_model = LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_topics)
+    # Apply preprocessing to each document
+    print("Prossesar dokument")
+    preprocessed_documents = [preprocess(doc) for doc in documents]
 
-# Print the topics and their keywords
-for topic_id, topic in lda_model.show_topics(num_topics=num_topics, num_words=10):
-    print(f"Topic #{topic_id}: {topic.encode('utf-8')}")
+    """
+    # Create bigram and trigram models
+    print("Gennererar bigram och trigram")
+    bigram = Phrases(preprocessed_documents, min_count=5, threshold=100)
+    trigram = Phrases(bigram[preprocessed_documents], min_count=5, threshold=100)
+
+    # Create a Phraser object for efficiency
+    bigram_mod = Phraser(bigram)
+    trigram_mod = Phraser(trigram)
+
+    # Apply bigram and trigram models to documents
+    preprocessed_documents = [trigram_mod[bigram_mod[doc]] for doc in preprocessed_documents]
+    """
+
+    # Create a dictionary and corpus for the LDA model
+    dictionary = Dictionary(preprocessed_documents)
+    corpus = [dictionary.doc2bow(doc) for doc in preprocessed_documents]
+
+    # Get the number of available cores
+    num_cores = multiprocessing.cpu_count()
+    print("Antal CPU Kärnor:",num_cores)
+    # Train the LDA model using LdaMulticore
+    start_time = time.time()
+    num_topics = 10
+    lda_model = LdaMulticore(corpus, num_topics, id2word=dictionary, passes=10, workers=num_cores)
+
+    end_time = time.time()
+
+    # Print the topics and their corresponding word probabilities
+    for idx, topic in lda_model.print_topics(-1):
+        print(f'Topic: {idx}')
+        print(f'Words: {topic}\n')
+    
+    # Get the keywords for each topic
+    topic_keywords = lda_model.show_topics(num_topics=num_topics, num_words=10)
+
+    # Extract the words from the topic keywords
+    new_stopwords = set()
+    for topic_id, keywords in topic_keywords:
+        words = keywords.split('"')[1::2]  # Extract words between double quotes
+        new_stopwords.update(words)
+
+    # Print the new words for the stop list
+    print("New words for the stop list:")
+    print(new_stopwords)
+
+    # Print the execution time
+    execution_time = end_time - start_time
+    print(f"\nExecution time: {execution_time:.2f} seconds")
+
+if __name__ == '__main__':
+    main()
